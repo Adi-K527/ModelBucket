e9e29d1649dff78b116d5757001ae96511a9e971
Adi-K527 <adikandakurtikar2@gmail.com>
2024-08-09 16:18:26 -0500

    edited dockerfile

--- modelAPITemplate/src/main.py
+++ modelAPITemplate/src/main.py
@@ -2,13 +2,20 @@ import json
 import boto3
 import joblib
 import os
+from mangum import Mangum
+from fastapi import FastAPI
 
+app = FastAPI()
 
-def lambda_handler(event, context):
-    
+@app.get("/")
+def status():
+    return {"status": "ok"}
+
+@app.post("/predict")
+def predict():
     bucket = boto3.resource('s3', 
                             aws_access_key_id     = os.getenv('MY_AWS_ACCESS_THING'), 
-                            aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS'  )).Bucket(os.getenv('AWS_BUCKET'))
+                            aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS' )).Bucket(os.getenv('AWS_BUCKET'))
     
     model_name = os.getenv("MODEL_NAME")
     bucket.download_file(model_name, model_name)
@@ -18,3 +25,9 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps({"model": model})
     }
+
+
+def lambda_handler(event, context):
+    return { 'statusCode': 200, 'body': "hi" }
+
+handler = Mangum(app)
