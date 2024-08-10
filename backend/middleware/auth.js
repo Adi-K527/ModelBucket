88cdf1d93abc10bfaadbccc30efddcfc089bf182
Adi-K527 <adikandakurtikar2@gmail.com>
2024-08-10 01:36:32 -0500

    Initial Backend

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,33 @@
 import jwt from "jsonwebtoken"
+import { client } from "../server.js"
+
+const secure = async (req, res, next) => {
+    try {
+        if (req.cookies.AUTH_TOKEN) {
+            const token = req.cookies.AUTH_TOKEN
+
+            const {id, username, email} = jwt.decode(token, process.env.JWT_SECRET)
+
+            const response = await client.query(
+                "SELECT id FROM users WHERE id = $1", 
+                [id]
+            )
+    
+            if (response.rows.length > 0) {
+                next()
+            }
+            else {
+                res.status(400).json({"error": "Invalid Credentials"})
+            }
+        }
+        else {
+            res.status(400).json({"error": "Invalid Credentials"})
+        }
+    }
+    catch (error) {
+        console.error(error)
+        res.status(400).json({"error": error})
+    }
+}
+
+export { secure }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from "express"
+import { login, register, getProfile, updateProfile } from "../controllers/userControllers.js"
+import { secure } from "../middleware/auth.js"
 
 const router = express.Router()
 
+router.post("/login",     login)
+router.post("/register",  register)
 
-router.get("/", async (req, res) => {
-    res.status(200).json({"message": "hi"})
-})
+router.get("/profile",    secure,   getProfile)
+router.put("/profile",    secure,   updateProfile)
 
 export default router
