cf3f8faabf25b20deafbd3e4ed19e48b76c8fa46
Adi-K527 <adikandakurtikar2@gmail.com>
2024-08-14 15:25:32 -0500

    deployment tier 1 fixes

--- modelAPITemplate/src/main.py
+++ modelAPITemplate/src/main.py
@@ -1,17 +1,17 @@
-import json
-import boto3
-import joblib
-import os
+# import json
+# import boto3
+# import joblib
+# import os
 from mangum import Mangum
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Any
-import numpy as np
+# from pydantic import BaseModel
+# from typing import Any
+# import numpy as np
 
 app = FastAPI()
 
-class BatchData(BaseModel):
-    data: Any
+# class BatchData(BaseModel):
+#     data: Any
     
 
 @app.get("/")
@@ -19,34 +19,34 @@ def status():
     return {"status": "ok"}
 
 
-@app.post("/predict")
-def predict(request: BatchData):
+# @app.post("/predict")
+# def predict(request: BatchData):
 
-    def np_encoder(object):
-        if isinstance(object, np.generic):
-            return object.item()
+#     def np_encoder(object):
+#         if isinstance(object, np.generic):
+#             return object.item()
         
-    bucket = boto3.resource('s3', 
-                            aws_access_key_id     = os.getenv('MY_AWS_ACCESS_THING'), 
-                            aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS' )).Bucket(os.getenv('AWS_BUCKET'))
+#     bucket = boto3.resource('s3', 
+#                             aws_access_key_id     = os.getenv('MY_AWS_ACCESS_THING'), 
+#                             aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS' )).Bucket(os.getenv('AWS_BUCKET'))
     
-    try:
-        model_name = os.getenv("MODEL_NAME")
-        bucket.download_file("models/" + model_name + ".joblib", "/tmp/model.joblib")
+#     try:
+#         model_name = os.getenv("MODEL_NAME")
+#         bucket.download_file("models/" + model_name + ".joblib", "/tmp/model.joblib")
 
-        model = joblib.load('/tmp/model.joblib')
-        prediction = model.predict([request.data])
+#         model = joblib.load('/tmp/model.joblib')
+#         prediction = model.predict([request.data])
 
-    except Exception as e:
-        return {
-            "statusCode": 400,
-            "error": str(type(e)),
-            "body": str(e)
-        }
+#     except Exception as e:
+#         return {
+#             "statusCode": 400,
+#             "error": str(type(e)),
+#             "body": str(e)
+#         }
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps({"prediction": prediction}, default=np_encoder)
-    }
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps({"prediction": prediction}, default=np_encoder)
+#     }
 
 handler = Mangum(app)
